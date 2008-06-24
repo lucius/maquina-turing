fcb8174cf366fbd30e0c634c3c1d6f3f56fca35c
vibortolotti <vibortolotti@232873fe-5a50-0410-86f5-dd2dbf440c40>
2008-06-24 00:56:21 +0000

    < Projeto Criado >

---
+++ trunk/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//#include "../userIncludes/ParserMaquina.h"
+//#include "../userIncludes/LogErros.h"
+
+int main ( int argc, char** argv )
+{
+	std::cout << "bogaFire";
+
+	return ( EXIT_SUCCESS );
+}
